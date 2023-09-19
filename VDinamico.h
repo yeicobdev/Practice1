4f2b7fb4b93eb49e7640a0bb8ca0ce2d841626e7
yeicobdev <124450350+yeicobdev@users.noreply.github.com>
2023-09-19 19:55:34 +0200

    Version3

--- VDinamico.h
+++ VDinamico.h
@@ -4,18 +4,68 @@
 
 #ifndef PRACTICE1_VDINAMICO_H
 #define PRACTICE1_VDINAMICO_H
+#include <algorithm>
 template <class T>
 class VDinamico{
     T* mem=nullptr;
     long int tam;
 public:
     VDinamico<T>(unsigned int n):tam(n){
+        mem=new T[n];
     };
     VDinamico<T>(const VDinamico<T>& origen);
     VDinamico<T>(const VDinamico<T>& origen,unsigned int posInicial,unsigned int numElementos);
     VDinamico<T>& operator=(const VDinamico<T>& origen);
-
+    T& operator[]( int pos);
+    ~VDinamico<T>();
+    void ordenar();
+    void ordenarRev();
+    int getTam()const;
             };
+
+template<class T>
+int VDinamico<T>::getTam() const {
+    return tam;
+}
+
+template<class T>
+void VDinamico<T>::ordenarRev() {
+    for(int i=0;i<tam;i++) {
+        for (int q = 0; q < tam - 1; q++) {
+            if (mem[q] < mem[q + 1]) {
+                T aux = mem[q];
+                mem[q] = mem[q + 1];
+                mem[q + 1] = aux;
+            }
+        }
+    }
+}
+
+template<class T>
+void VDinamico<T>::ordenar() {
+for(int i=0;i<tam-1;i++)
+{
+    for(int j=i+1;j<tam-i;j++)
+    {
+        if(mem[i]>mem[j])
+        {
+            T aux=mem[i];
+            mem[i]=mem[j];
+            mem[j]=aux;
+        }
+    }
+}
+}
+
+template<class T>
+VDinamico<T>::~VDinamico<T>() {
+delete[] mem;
+}
+
+template<class T>
+T& VDinamico<T>::operator[](int pos) {
+    return mem[pos];
+}
 
 template<class T>
 VDinamico<T>& VDinamico<T>::operator=(const VDinamico<T> &origen) {
--- main.cpp
+++ main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include "VDinamico.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+VDinamico<int>* p=new VDinamico<int> (20);
+for(int i=0;i<p->getTam();i++){
+    (*p)[i]=i;
+}
+for(int i=0;i<20;i++){
+    std::cout<<(*p)[i]<<" ";
+}
+std::cout<<std::endl;
+p->ordenar();
+for(int i=0;i<20;i++){
+    std::cout<<(*p)[i]<<" ";
+}
+std::cout<<std::endl;
+p->ordenarRev();
+for(int i=0;i<20;i++){
+    std::cout<<(*p)[i]<<" ";
+}
+delete p;
+
+
+
+
+
     return 0;
 }
