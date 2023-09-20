cbe3a8d5b0a8ae6401c8f3fff05c8a986d4788c5
yeicobdev <124450350+yeicobdev@users.noreply.github.com>
2023-09-20 20:52:33 +0200

    Version3

--- VDinamico.h
+++ VDinamico.h
@@ -10,12 +10,67 @@ class VDinamico{
     long int tam;
 public:
     VDinamico<T>(unsigned int n):tam(n){
+        mem=new T[tam];
     };
     VDinamico<T>(const VDinamico<T>& origen);
     VDinamico<T>(const VDinamico<T>& origen,unsigned int posInicial,unsigned int numElementos);
     VDinamico<T>& operator=(const VDinamico<T>& origen);
+    T& operator[](int pos);
+    void ordenar();
+    void ordenarRev();
+    int getTam()const;
+    void swap(T a,T b);
 
             };
+
+template<class T>
+void VDinamico<T>::swap(T a, T b) {
+    T aux=a;
+    a=b;
+    b=aux;
+}
+
+template<class T>
+int VDinamico<T>::getTam() const {
+    return tam;
+}
+
+template<class T>
+void VDinamico<T>::ordenarRev() {
+    for(int i=0;i<tam;i++) {
+        for (int j = 0; j < tam - 1; j++) {
+            if (mem[j] < mem[j + 1]) {
+                swap(mem[j], mem[j + 1]);
+            }
+        }
+    }
+}
+
+template<class T>
+void VDinamico<T>::ordenar() {
+    bool flag;
+    for(int i = 0; i<tam; i++)
+    {
+        flag = false;
+        for(int j = 0; j < tam-i-1; j++)
+        {
+            if( mem[j] > mem[j+1])
+            {
+                swap(mem[j],mem[j+1]);
+                flag = true;
+            }
+        }
+        if(!flag)
+        {
+            break;
+        }
+    }
+}
+
+template<class T>
+T &VDinamico<T>::operator[](int pos) {
+    return mem[pos];
+}
 
 template<class T>
 VDinamico<T>& VDinamico<T>::operator=(const VDinamico<T> &origen) {
--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
-
+#include "VDinamico.h"
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    VDinamico<int>* p=new VDinamico<int>(20);
+    for(int i=0;i<p->getTam();i++)
+    {
+        (*p)[i]=i;
+    }
+    for(int i=0;i<p->getTam();i++)
+    {
+        std::cout<<(*p)[i]<<" ";
+    }
+    std::cout<<std::endl;
+    p->ordenar();
+    for(int i=0;i<p->getTam();i++)
+    {
+        std::cout<<(*p)[i]<<" ";
+    }
+    std::cout<<std::endl;
+    p->ordenarRev();
+    for(int i=0;i<p->getTam();i++)
+    {
+        std::cout<<(*p)[i]<<" ";
+    }
+    delete p;
+
 }
