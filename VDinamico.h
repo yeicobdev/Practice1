4e40747dca6bdcdb7e1cf469041c94886b36b3fa
yeicobdev <124450350+yeicobdev@users.noreply.github.com>
2023-09-22 13:11:08 +0200

    Version4

--- VDinamico.h
+++ VDinamico.h
@@ -9,9 +9,7 @@ class VDinamico{
     T* mem=nullptr;
     long int tam;
 public:
-    VDinamico<T>(unsigned int n):tam(n){
-        mem=new T[tam];
-    };
+    VDinamico<T>(int n);
     VDinamico<T>(const VDinamico<T>& origen);
     VDinamico<T>(const VDinamico<T>& origen,unsigned int posInicial,unsigned int numElementos);
     VDinamico<T>& operator=(const VDinamico<T>& origen);
@@ -23,7 +21,26 @@ public:
 
             };
 
+void filtroNConstructor(int n){
+    if(n<=0)
+    {
+        throw std::invalid_argument("0 o menor");
+    }
+}
+template<class T>
+void filtroMemoriaConstructor(T* p){
+    if(p= nullptr){
+        throw std::bad_alloc();
+    }
+}
 
+
+template<class T>
+VDinamico<T>::VDinamico(int n):tam(n) {
+    filtroNConstructor(tam);
+    mem=new T[tam];
+    filtroMemoriaConstructor(mem);
+};
 
 
 template<class T>
@@ -69,6 +86,10 @@ void VDinamico<T>::ordenar() {
 
 template<class T>
 T &VDinamico<T>::operator[](int pos) {
+    if((pos>=tam)||(pos<0))
+    {
+        throw std::invalid_argument("pos invalida");
+    }
     return mem[pos];
 }
 
@@ -89,7 +110,15 @@ VDinamico<T>& VDinamico<T>::operator=(const VDinamico<T> &origen) {
 
 template<class T>
 VDinamico<T>::VDinamico(const VDinamico<T> &origen, unsigned int posInicial, unsigned int numElementos) {
+    if((posInicial<0)||(posInicial>numElementos)||posInicial>=origen.tam)
+    {
+        throw std::invalid_argument("Fallo en posInicial");
+    }
+    if((numElementos>origen.tam)||(numElementos<=0)){
+        throw std::invalid_argument("Fallo en numElementos");
+    }
     mem=new T[numElementos];
+    filtroMemoriaConstructor(mem);
     for(int i=0;i<numElementos;i++)
     {
         mem[i]= origen.mem[i+posInicial];
@@ -98,11 +127,13 @@ VDinamico<T>::VDinamico(const VDinamico<T> &origen, unsigned int posInicial, uns
 
 template<class T>
 VDinamico<T>::VDinamico(const VDinamico<T> &origen) {
+    filtroNConstructor(origen.tam);
     mem=new T[origen.tam];
-for(int i=0;i<tam;i++)
-{
-    mem[i]= origen.mem[i];
-}
+    filtroMemoriaConstructor(mem);
+    for(int i=0;i<tam;i++)
+    {
+        mem[i]= origen.mem[i];
+    }
 }
 
 
--- main.cpp
+++ main.cpp
@@ -1,27 +1,27 @@
 #include <iostream>
 #include "VDinamico.h"
 int main() {
-    VDinamico<int>* p=new VDinamico<int>(20);
-    for(int i=0;i<p->getTam();i++)
-    {
-        (*p)[i]=i;
+    try {
+        VDinamico<int> *p = new VDinamico<int>(20);
+        for (int i = 0; i < p->getTam(); i++) {
+            (*p)[i] = i;
+        }
+        for (int i = 0; i < p->getTam(); i++) {
+            std::cout << (*p)[i] << " ";
+        }
+        std::cout << std::endl;
+        p->ordenar();
+        for (int i = 0; i < p->getTam(); i++) {
+            std::cout << (*p)[i] << " ";
+        }
+        std::cout << std::endl;
+        p->ordenarRev();
+        for (int i = 0; i < p->getTam(); i++) {
+            std::cout << (*p)[i] << " ";
+        }
+        delete p;
+    }catch(std::exception& e){
+        std::cout<<e.what()<<std::endl;
     }
-    for(int i=0;i<p->getTam();i++)
-    {
-        std::cout<<(*p)[i]<<" ";
-    }
-    std::cout<<std::endl;
-    p->ordenar();
-    for(int i=0;i<p->getTam();i++)
-    {
-        std::cout<<(*p)[i]<<" ";
-    }
-    std::cout<<std::endl;
-    p->ordenarRev();
-    for(int i=0;i<p->getTam();i++)
-    {
-        std::cout<<(*p)[i]<<" ";
-    }
-    delete p;
 
 }
