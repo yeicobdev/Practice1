cf03a122a46942a9370e16a9e828ae1626539df9
yeicobdev <124450350+yeicobdev@users.noreply.github.com>
2023-10-20 01:14:48 +0200

    Version2.5

--- main.cpp
+++ main.cpp
@@ -1,106 +1,71 @@
 #include "Aeropuerto.h"
 #include "VDinamico.h"
+#include "ListaEnlazada.h"
 #include <fstream>
 #include <string>
 #include <sstream>
 
 /**  @author Jacobo Pancorbo Bianquetti jpb00023@red.ujaen.es      */
 int main() {
-VDinamico<Aeropuerto> aeropuertos=VDinamico<Aeropuerto>();
-    std::ifstream is;
-    std::stringstream  columnas;
-    std::string fila;
-    std::string id = "";
-    std::string ident="";
-    std::string tipo="";
-    std::string nombre="";
-    std::string latitud_str="";
-    std::string longitud_str="";
-    std::string continente="";
-    std::string iso_pais="";
-    int contador=0;
-    is.open("C://Users//jacob//OneDrive//Escritorio//EEDD//Practice1//aeropuertos.csv");
-    if ( is.good() ) {
-
-        clock_t t_ini = clock();
-
-        while (( getline(is, fila ) )&&(contador<5000)){
-
-            if (fila!="") {
-
-                columnas.str(fila);
-
-                //formato de fila: id;ident;tipo;nombre;latitud;longitud;continente;iso_pais
-
-                getline(columnas, id, ';'); //leemos caracteres hasta encontrar y omitir ';'
-                getline(columnas,ident,';');
-                getline(columnas,tipo,';');
-                getline(columnas,nombre,';');
-                getline(columnas,latitud_str,';');
-                getline(columnas,longitud_str,';');
-                getline(columnas,continente,';');
-                getline(columnas,iso_pais,';');
-
-                //  Transformamos la latitud y longitud a float
-                float latitud=std::stof(latitud_str);
-                float longitud=std::stof(longitud_str);
-                aeropuertos.insertar( *new Aeropuerto(id,ident,tipo,nombre, *(new UTM(latitud,longitud)),continente,iso_pais),contador);
-
-                fila="";
-                columnas.clear();
-
-            }
-            contador++;
-        }
-
-        is.close();
-
-        std::cout << "Tiempo lectura: " << ((clock() - t_ini) / (float) CLOCKS_PER_SEC) << " segs." << std::endl;
-    } else {
-        std::cout << "Error de apertura en archivo" << std::endl;
-    }
-    std::cout<<std::endl;
-    std::cout<<std::endl;
-    aeropuertos.ordenarRev();
-    for(int i=0;i<30;i++)
+    //Prueba 1
+ListaEnlazada<int>* listaPrueba=new ListaEnlazada<int>;
+for(int i=101;i<201;i++){
+    listaPrueba->insertaFin(i);
+}
+for(int i=98;i>0;i--){
+    listaPrueba->insertaInicio(i);
+}
+Iterador<int>* iteradorPrueba=new Iterador<int>(listaPrueba->getCabecera());
+while(iteradorPrueba->dato()!=101){
+    iteradorPrueba->siguiente();
+}
+listaPrueba->inserta(*iteradorPrueba,100);
+iteradorPrueba->setNodo(listaPrueba->getCabecera());
+while(iteradorPrueba->dato()!=98){
+    iteradorPrueba->siguiente();
+}
+listaPrueba->insertaDetras(*iteradorPrueba,99);
+iteradorPrueba->setNodo(listaPrueba->getCabecera());
+std::cout<<"Hacemos las inserciones: "<<std::endl;
+while(!iteradorPrueba->fin())
+{
+    std::cout<<iteradorPrueba->dato()<<" ";
+    iteradorPrueba->siguiente();
+}
+std::cout<<std::endl;
+std::cout<<"Borramos los 10 primeros y los 10 Ultimos datos:"<<std::endl;
+for(int i=0;i<10;i++)
+{
+    listaPrueba->borraInicio();
+}
+for(int i=0;i<10;i++)
+{
+    listaPrueba->borraFinal();
+}
+iteradorPrueba->setNodo(listaPrueba->getCabecera());
+while(!iteradorPrueba->fin())
+{
+    std::cout<<iteradorPrueba->dato()<<" ";
+    iteradorPrueba->siguiente();
+}
+std::cout<<std::endl;
+std::cout<<"Borramos los multiplos de 10:"<<std::endl;
+iteradorPrueba->setNodo(listaPrueba->getCabecera());
+while(!iteradorPrueba->fin()){
+    if(((iteradorPrueba->dato())%10)==0)
     {
-        std::cout<<(aeropuertos[i]).getId()<<std::endl;
+        listaPrueba->borra(*iteradorPrueba);//mi funcion borra hace que se pase al siguiente nodo automáticamente
+    }else{
+        iteradorPrueba->siguiente();
     }
-    std::cout<<std::endl;
-    std::cout<<std::endl;
-    aeropuertos.ordenar();
-    for(int i=0;i<30;i++)
-    {
-        std::cout<<(aeropuertos[i]).getId()<<std::endl;
-    }
-    std::cout<<std::endl;
-    std::cout<<std::endl;
-    std::cout<<"Tamano fisico antes de borrar: "<<aeropuertos.getTamFis()<<std::endl;
-    std::cout<<"Tamano logico antes de borrar: "<<aeropuertos.getTamLog()<<std::endl;
-    int borrados=0;
-    for( int i=0;i<aeropuertos.getTamLog();i++)
-    {
-        if(aeropuertos[i].getContinente()=="NA")
-        {
-            aeropuertos.borrar(i);
-            borrados++;
-        }
-    }
-    std::cout<<"Tamano fisico despues de borrar: "<<aeropuertos.getTamFis()<<std::endl;
-    std::cout<<"Tamano logico despues de borrar: "<<aeropuertos.getTamLog()<<std::endl;
-    std::cout<<std::endl;
-    std::cout<<std::endl;
-    Aeropuerto* a1=new Aeropuerto("345166");
-    std::cout<<"345166: "<<aeropuertos.busquedaBin(*a1)<<std::endl;
-    delete a1;
-    Aeropuerto* a2=new Aeropuerto("9999");
-    std::cout<<"9999: "<<aeropuertos.busquedaBin(*a2)<<std::endl;
-    delete a2;
-    Aeropuerto* a3=new Aeropuerto("0000");
-    std::cout<<"0000: "<<aeropuertos.busquedaBin(*a3)<<std::endl;
-    delete a3;
 
-
+}
+iteradorPrueba->setNodo(listaPrueba->getCabecera());
+while(!iteradorPrueba->fin())
+{
+    std::cout<<iteradorPrueba->dato()<<" ";
+    iteradorPrueba->siguiente();
+}
 
     return 0;
 
